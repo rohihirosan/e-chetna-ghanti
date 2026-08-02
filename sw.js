2396922ebc00e78ba005e5c8da89f2cb9050b3dc
rohihirosan <rkkaemail@gmail.com>
2026-08-02 13:40:15 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = 'prarthana-app-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './prayer.html',
+  './manifest.json'
+];
+
+// इंस्टॉल इवेंट
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+// फेच इवेंट
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
